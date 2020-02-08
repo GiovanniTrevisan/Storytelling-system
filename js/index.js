9bcac561a15c2de7da80eaaa37c96ada312f7a02
gitre <gitrevizzo@hotmail.com.br>
2020-02-08 17:14:27 -0300

    feature//Design-done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
-$(".background").click(function(){
-    $(".gradient-1").toggleClass("hide");
-    $(".gradient-2").toggleClass("hide");
-  });
+$(document).ready(function(){
+$("button").click(function () {
+  $(".gradient-1").toggleClass("hide");
+  $(".gradient-2").toggleClass("hide");
+});
 
-  
+$( ".content" ).toggleClass( "hide" );
+
+var story = {
+  title: "Hist",
+   content: "Content",
+    paths:{ meeh: "Hist", jooj: "Content"}
+};
+
+
+});
