1c84fc4e33b73f63ad32688c7f7bb9a25f6b8719
gitre <gitrevizzo@hotmail.com.br>
2020-02-08 21:18:32 -0300

    feature//Updates-Story-System

--- js/index.js
+++ js/index.js
@@ -1,31 +1,98 @@
 $(document).ready(function () {
 
 
-  var story = {
-    title: "História",
-    content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto et facilis voluptatum reiciendis labore, dicta distinctio doloribus fuga eveniet placeat adipisci corporis animi alias repudiandae nobisneque! Enim, voluptatibus quasi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem pariatur odit sequi accusamus voluptate incidunt eaque, ex perspiciatis qui velit! Voluptate magni numquam similique doloremque magnam dolorem nulla reiciendis molestiae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quae dignissimos alias laborum totam  sint mollitia qui minus cupiditate consectetur! Est recusandae voluptatibus quos. Voluptates nisi repellendus exercitationem accusamus quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur totam similique saepe ad commodideserunt in! Inventore sequi, doloremque eligendi sint sapiente, quo velit optio hic debitis voluptatesadipisci earum.",
-    paths: [
-      {icon: "https://img.icons8.com/ios/35/FFFFFF/sword.png", message: "Lorem ipsum, dolor sit amet consectetur." },    
-      {icon: "https://img.icons8.com/windows/35/FFFFFF/shield.png", message: "Lorem ipsum, dolor sit amet consectetur." }
-    ]
-  };
-  function changeText() {
+    var story = {
+        title: "História",
+        content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto et facilis voluptatum reiciendis labore, dicta distinctio doloribus fuga eveniet placeat adipisci corporis animi alias repudiandae nobisneque! Enim, voluptatibus quasi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem pariatur odit sequi accusamus voluptate incidunt eaque, ex perspiciatis qui velit! Voluptate magni numquam similique doloremque magnam dolorem nulla reiciendis molestiae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quae dignissimos alias laborum totam  sint mollitia qui minus cupiditate consectetur! Est recusandae voluptatibus quos. Voluptates nisi repellendus exercitationem accusamus quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur totam similique saepe ad commodideserunt in! Inventore sequi, doloremque eligendi sint sapiente, quo velit optio hic debitis voluptatesadipisci earum.",
+        paths: [{
+            icon: "",
+            message: "Lorem ipsum, dolor sit amet consectetur.",
+            path: {
+                title: "História 1",
+                content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto et facilis voluptatum reiciendis labore, dicta distinctio doloribus fuga eveniet placeat adipisci corporis animi alias repudiandae nobisneque! Enim, voluptatibus quasi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem pariatur odit sequi accusamus voluptate incidunt eaque, ex perspiciatis qui velit! Voluptate magni numquam similique doloremque magnam dolorem nulla reiciendis molestiae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quae dignissimos alias laborum totam  sint mollitia qui minus cupiditate consectetur! Est recusandae voluptatibus quos. Voluptates nisi repellendus exercitationem accusamus quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur totam similique saepe ad commodideserunt in! Inventore sequi, doloremque eligendi sint sapiente, quo velit optio hic debitis voluptatesadipisci earum.",
+                paths: [{
+                    icon: "https://img.icons8.com/ios/35/FFFFFF/sword.png",
+                    message: "Lorem ipsum, dolor sit amet consectetur.",
+                    path: {
+                        title: "História 3",
+                        content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto et facilis voluptatum reiciendis labore, dicta distinctio doloribus fuga eveniet placeat adipisci corporis animi alias repudiandae nobisneque! Enim, voluptatibus quasi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem pariatur odit sequi accusamus voluptate incidunt eaque, ex perspiciatis qui velit! Voluptate magni numquam similique doloremque magnam dolorem nulla reiciendis molestiae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quae dignissimos alias laborum totam  sint mollitia qui minus cupiditate consectetur! Est recusandae voluptatibus quos. Voluptates nisi repellendus exercitationem accusamus quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur totam similique saepe ad commodideserunt in! Inventore sequi, doloremque eligendi sint sapiente, quo velit optio hic debitis voluptatesadipisci earum.",
+                        paths: [{
+                            icon: "https://img.icons8.com/ios/35/FFFFFF/sword.png",
+                            message: "Lorem ipsum, dolor sit amet consectetur.",
+                            path: {
+                                title: "História 4",
+                                content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto et facilis voluptatum reiciendis labore, dicta distinctio doloribus fuga eveniet placeat adipisci corporis animi alias repudiandae nobisneque! Enim, voluptatibus quasi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem pariatur odit sequi accusamus voluptate incidunt eaque, ex perspiciatis qui velit! Voluptate magni numquam similique doloremque magnam dolorem nulla reiciendis molestiae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quae dignissimos alias laborum totam  sint mollitia qui minus cupiditate consectetur! Est recusandae voluptatibus quos. Voluptates nisi repellendus exercitationem accusamus quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur totam similique saepe ad commodideserunt in! Inventore sequi, doloremque eligendi sint sapiente, quo velit optio hic debitis voluptatesadipisci earum.",
+        
+                            }
+                        },{
+                            icon: "https://img.icons8.com/ios/35/FFFFFF/sword.png",
+                            message: "Lorem ipsum, dolor sit amet consectetur.",
+                            path: {
+                                title: "História 8",
+                                content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto et facilis voluptatum reiciendis labore, dicta distinctio doloribus fuga eveniet placeat adipisci corporis animi alias repudiandae nobisneque! Enim, voluptatibus quasi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem pariatur odit sequi accusamus voluptate incidunt eaque, ex perspiciatis qui velit! Voluptate magni numquam similique doloremque magnam dolorem nulla reiciendis molestiae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quae dignissimos alias laborum totam  sint mollitia qui minus cupiditate consectetur! Est recusandae voluptatibus quos. Voluptates nisi repellendus exercitationem accusamus quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur totam similique saepe ad commodideserunt in! Inventore sequi, doloremque eligendi sint sapiente, quo velit optio hic debitis voluptatesadipisci earum.",
+        
+                            }
+                        },{
+                            icon: "https://img.icons8.com/ios/35/FFFFFF/sword.png",
+                            message: "Lorem ipsum, dolor sit amet consectetur.",
+                            path: {
+                                title: "História 9",
+                                content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto et facilis voluptatum reiciendis labore, dicta distinctio doloribus fuga eveniet placeat adipisci corporis animi alias repudiandae nobisneque! Enim, voluptatibus quasi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem pariatur odit sequi accusamus voluptate incidunt eaque, ex perspiciatis qui velit! Voluptate magni numquam similique doloremque magnam dolorem nulla reiciendis molestiae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quae dignissimos alias laborum totam  sint mollitia qui minus cupiditate consectetur! Est recusandae voluptatibus quos. Voluptates nisi repellendus exercitationem accusamus quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur totam similique saepe ad commodideserunt in! Inventore sequi, doloremque eligendi sint sapiente, quo velit optio hic debitis voluptatesadipisci earum.",
+        
+                            }
+                        }]
+                    }
+                }]
+            }
+        },
+        {
+            icon: "https://img.icons8.com/windows/35/FFFFFF/shield.png",
+            message: "Lorem ipsum, dolor sit amet consectetur.",
+            path: {
+                title: "História 2",
+                content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto et facilis voluptatum reiciendis labore, dicta distinctio doloribus fuga eveniet placeat adipisci corporis animi alias repudiandae nobisneque! Enim, voluptatibus quasi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem pariatur odit sequi accusamus voluptate incidunt eaque, ex perspiciatis qui velit! Voluptate magni numquam similique doloremque magnam dolorem nulla reiciendis molestiae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quae dignissimos alias laborum totam  sint mollitia qui minus cupiditate consectetur! Est recusandae voluptatibus quos. Voluptates nisi repellendus exercitationem accusamus quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur totam similique saepe ad commodideserunt in! Inventore sequi, doloremque eligendi sint sapiente, quo velit optio hic debitis voluptatesadipisci earum.",
+                paths: [{
+                    icon: "https://img.icons8.com/ios/35/FFFFFF/sword.png",
+                    message: "Lorem ipsum, dolor sit amet consectetur.",
+                    path: {
+                        title: "História 5",
+                        content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto et facilis voluptatum reiciendis labore, dicta distinctio doloribus fuga eveniet placeat adipisci corporis animi alias repudiandae nobisneque! Enim, voluptatibus quasi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem pariatur odit sequi accusamus voluptate incidunt eaque, ex perspiciatis qui velit! Voluptate magni numquam similique doloremque magnam dolorem nulla reiciendis molestiae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quae dignissimos alias laborum totam  sint mollitia qui minus cupiditate consectetur! Est recusandae voluptatibus quos. Voluptates nisi repellendus exercitationem accusamus quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur totam similique saepe ad commodideserunt in! Inventore sequi, doloremque eligendi sint sapiente, quo velit optio hic debitis voluptatesadipisci earum.",
+                        paths: [{
+                            icon: "https://img.icons8.com/ios/35/FFFFFF/sword.png",
+                            message: "Lorem ipsum, dolor sit amet consectetur.",
+                            path: {
+                                title: "História 5",
+                                content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto et facilis voluptatum reiciendis labore, dicta distinctio doloribus fuga eveniet placeat adipisci corporis animi alias repudiandae nobisneque! Enim, voluptatibus quasi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem pariatur odit sequi accusamus voluptate incidunt eaque, ex perspiciatis qui velit! Voluptate magni numquam similique doloremque magnam dolorem nulla reiciendis molestiae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quae dignissimos alias laborum totam  sint mollitia qui minus cupiditate consectetur! Est recusandae voluptatibus quos. Voluptates nisi repellendus exercitationem accusamus quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur totam similique saepe ad commodideserunt in! Inventore sequi, doloremque eligendi sint sapiente, quo velit optio hic debitis voluptatesadipisci earum.",
+        
+                            }
+                        }]
+                    }
+                }]
+            }
+        }
+        ]
+    };
 
-    $(".title").text(story.title);
+    function changeText(story) {
 
-    $(".content-paragraph").text(story.content);
+        $(".title").text(story.title);
 
-    $(".choose-path").empty();
+        $(".content-paragraph").text(story.content);
 
-    $.each(story.paths, function(index, value) {
-      $(".choose-path").append('<button class="col-lg-5 col-sm-12 btn button text-light" path="' + index + '"><img class="icon"    src="' + value.icon + '">    ' + value.message + '</button>');
-    });
-  }
-  changeText();
+        $(".choose-path").empty();
 
-  $("button").click(function () {
-    $(".gradient-1").toggleClass("hide");
-    $(".gradient-2").toggleClass("hide");
-  });
+        if(!story.paths){
+            $(".title").text(story.title);
+        }
+        $.each(story.paths, function (index, value) {
+            $(".choose-path").append('<button class="col-lg-4 col-sm-12 btn button text-light" path="' + index + '"><img class="icon"    src="' + value.icon + '">    ' + value.message + '</button>');
+        });
 
+        $("button").click(function () {
+            $(".gradient-1").toggleClass("hide");
+            $(".gradient-2").toggleClass("hide");
+            changeText(story.paths[$(this).attr("path")].path);
+        });
+    }
+    changeText(story);
 });
